2422c94b8875e709874ad6b8dd652950326ca5f8
Paul Skelton <paul.skelton@p40nkc64vp.home>
2022-10-21 17:09:00 +0100

    changes from work

--- server.py
+++ server.py
@@ -1,11 +1,11 @@
 from urllib.parse import urlencode, urlunsplit
 from flask import Flask, redirect, render_template, request, session, url_for
 from configparser import ConfigParser
-import requests, json, jwt, time, secrets, urllib.parse
+import requests, json, jwt, time, secrets, urllib.parse, pkce
 
 svr = Flask(__name__)
 config_object = ConfigParser()
-config_object.read("./config/demo_uat_2.ini")
+config_object.read("config/tc_stage.ini")
 
 svr.secret_key = secrets.token_urlsafe(16)
 
@@ -22,10 +22,10 @@ USERINFO_PATH   = config_object["IDP"]["USERINFO_PATH"]
 API_DOMAIN          = config_object["API"]["DOMAIN"]
 CONTACT_PATH        = config_object["API"]["CONTACT_PATH"]
 CONTACT_CLIENT_ID   = config_object["API"]["CONTACT_CLIENT_ID"]
-CONTACT_SECRET     = config_object["API"]["CONTACT_SECRET"]
+CONTACT_SECRET      = config_object["API"]["CONTACT_SECRET"]
 CONSENT_PATH        = config_object["API"]["CONSENT_PATH"]
 CONSENT_CLIENT_ID   = config_object["API"]["CONSENT_CLIENT_ID"]
-CONSENT_SECRET     = config_object["API"]["CONSENT_SECRET"]
+CONSENT_SECRET      = config_object["API"]["CONSENT_SECRET"]
 
 @svr.route("/")
 def index() :
@@ -40,6 +40,7 @@ def index() :
 
 @svr.route("/callback")
 def callback() :
+
     code = request.args.get("code")
     state = request.args.get("state")
 
@@ -51,25 +52,12 @@ def callback() :
 def exchange() :
     return callTokenEndpoint('authorization_code')
 
-def extractTokenPlusExpiryFromSession(tokenName) :
-    if tokenName in session :
-        token = session[tokenName]
-        try :
-            token_claims = jwt.decode(token, options={"verify_signature": False})
-            token_expiry_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(token_claims['exp']))
-        except Exception as e:
-            token_expiry_time  = "Unable to decode token. Not a JWT?"
-    else :
-        token = 'No token present'
-        token_expiry_time = ''
-
-    return token, token_expiry_time
 
 @svr.route("/displayTokens")
 def displayTokens() :
-    access_token, access_token_expiry_time = extractTokenPlusExpiryFromSession('access_token')
-    id_token, id_token_expiry_time = extractTokenPlusExpiryFromSession('id_token')
-    refresh_token, refresh_token_expiry_time = extractTokenPlusExpiryFromSession('refresh_token')
+    access_token, access_token_expiry_time = extractTokenAndExpiryFromSession('access_token')
+    id_token, id_token_expiry_time = extractTokenAndExpiryFromSession('id_token')
+    refresh_token, refresh_token_expiry_time = extractTokenAndExpiryFromSession('refresh_token')
 
     return render_template('displayTokens.html', access_token = access_token, access_token_expiry = access_token_expiry_time, id_token = id_token, id_token_expiry = id_token_expiry_time, refresh_token = refresh_token, refresh_token_expiry = refresh_token_expiry_time)
 
@@ -113,6 +101,20 @@ def callContactEndpoint() :
 def callConsentlEndpoint() :
     return callExternalEndpoint(CONSENT_CLIENT_ID, CONSENT_SECRET, CONSENT_PATH)
 
+def extractTokenAndExpiryFromSession(tokenName) :
+    if tokenName in session :
+        token = session[tokenName]
+        try :
+            token_claims = jwt.decode(token, options={"verify_signature": False})
+            token_expiry_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(token_claims['exp']))
+        except Exception as e:
+            token_expiry_time  = "Unable to decode token. Not a JWT?"
+    else :
+        token = 'No token present'
+        token_expiry_time = ''
+
+    return token, token_expiry_time
+
 def callExternalEndpoint(clientid, clientsecret, path) :
     
     if ('access_token' not in session) :
@@ -139,12 +141,19 @@ def makeGETCall(domain, path, headers, params) :
     return r.json()
 
 def createAuthorizePath() :
+
+    code_verifier, code_challenge = pkce.generate_pkce_pair()
+
+    session['code_verifier'] = code_verifier
+
     params = {
         'client_id': CLIENT_ID,
         'scope': SCOPE,
         'response_type': 'code',
         'redirect_uri': REDIRECT_URI,
-        'state': svr.secret_key
+        'state': svr.secret_key,
+        'code_challenge' : code_challenge,
+        'code_challenge_method' : 'S256'
     }
     return urlunsplit(('https', IDP_DOMAIN, AUTHORIZE_PATH, urlencode(params), ""))
 
@@ -154,7 +163,8 @@ def callTokenEndpoint(grant_type) :
         'client_id'     : CLIENT_ID,
         'client_secret' : CLIENT_SECRET,
         'redirect_uri'  : REDIRECT_URI,
-        'grant_type'    : 'authorization_code'}
+        'grant_type'    : 'authorization_code',
+        'code_verifier' : session['code_verifier']}
     if(grant_type == 'refresh_token') :
         data = {'refresh_token' : session['refresh_token'],  
             'client_id'     : CLIENT_ID,
@@ -163,22 +173,17 @@ def callTokenEndpoint(grant_type) :
             'grant_type'    : 'refresh_token'}
 
     url = urlunsplit(('https', IDP_DOMAIN, TOKEN_PATH, '', ''))
-
     r = requests.post(url, data = data)
-
+    print(url)
+    print(data)
     if(r.status_code != 200) :
         return(str(r.status_code) + ' ' + r.text)
 
     payload = json.loads(r.text)
 
-    if 'access_token' in payload :
-        session['access_token'] = payload['access_token']
-
-    if 'id_token' in payload :
-        session['id_token'] = id_token = payload['id_token']
-
-    if 'refresh_token' in payload :
-        session['refresh_token'] = payload['refresh_token']
+    if 'access_token' in payload    : session['access_token'] = payload['access_token']
+    if 'id_token' in payload        : session['id_token'] = id_token = payload['id_token']
+    if 'refresh_token' in payload   : session['refresh_token'] = payload['refresh_token']
 
     return redirect(url_for('displayTokens'))
 
